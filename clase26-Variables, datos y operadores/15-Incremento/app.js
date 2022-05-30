0c9e70fcfb8f05894fc8bc58568e1331c2f80b69
AnyiGP <angelesgp31@hotmail.com>
2022-05-29 21:05:22 -0300

    Avanzando con ejercicios de JS

--- clase26-Variables, datos y operadores/15-Incremento/app.js
+++ clase26-Variables, datos y operadores/15-Incremento/app.js
@@ -1,20 +1,14 @@
 //Crear un programa que pida ingresar una número de partida y una cantidad a ir incrementando y mostrar cinco mensajes seguidos que muestren el número del mensaje y el total incrementado hasta el momento (p. ej. empezando con 5 e incrementando 5, Incremento 1) Total 10, Incremento 2) Total 15, Incremento 3) Total 20, etc.).
 
 const numeroDePartida = prompt('Ingresa un numero de partida')
-let incremento = incremento + 10
+let incremento = prompt('Ingresa la cantidad que quieres incrementar')
 
+let mensajeUno = alert (`Mensaje 1 Total ${Number(numeroDePartida) + Number(incremento)}`)
 
+let mensajeDos = alert (`Mensaje 2 Total ${Number(numeroDePartida) + Number(incremento * 2)}`)
 
-// const escalaDuracion1 = prompt('Ingresa el tiempo del vuelo en minutos')
-
-// const escala2 = prompt('Ingresa siguiente destino del vuelo')
-// const escalaDuracion2 = prompt('Ingresa el tiempo del vuelo en minutos')
-
-// const escala3 = prompt('Ingresa el ultimo desino de tu vielo')
-// const escalaDuracion3 = prompt('Ingresa el tiempo del vuelo en minutos')
-
-// const duracionTotal = (Number(escalaDuracion3) + Number(escalaDuracion2) + Number(escalaDuracion1))
-
-// alert (`Destino 1: ${escala1}, duracion ${escalaDuracion1} minutos. Destino 2: ${escala2}, duracion ${escalaDuracion2} minutos. Destino 3: ${escala3}, duracion ${escalaDuracion3} minutos. Duracion total del viaje ${duracionTotal} minutos`)
+let mensajeTres = alert (`Mensaje 3 Total ${Number(numeroDePartida) + Number(incremento * 3)}`)
+let mensajeCuatro = alert (`Mensaje 4 Total ${Number(numeroDePartida) + Number(incremento * 4)}`)
+let mensajeCinco = alert (`Mensaje 5 Total ${Number(numeroDePartida) + Number(incremento * 5)}`)
 
 
